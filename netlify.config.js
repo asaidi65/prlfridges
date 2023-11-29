46661d21229b5066b64c617554e9225b207169e1
Your Name <vavas@gmail.ahmed saidicom>
2023-11-29 10:53:38 +0000

    Initial commit

--- netlify.config.js
+++ netlify.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  build: {
-    publish: ".",
-  },
- 
-};
+[[redirects]] # Optional: Configure redirects for client-side routing
+  from = "/*"
+  to = "/views/index.ejs" # Update this line to point to your index.ejs file
+  status = 200
